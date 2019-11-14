f4f78e7029813dd818012f9f7c64fb67b81f5b89
mickadamouse1 <michaelchambers22@hotmail.co.uk>
2019-11-14 18:57:43 +0000

    Goblin Catcher: game now functions and is playable

--- Goblin Catcher/index.js
+++ Goblin Catcher/index.js
@@ -49,6 +49,8 @@ function checkLoaded() {
   ) {
     loaded = true;
     console.log("loaded.");
+    reset();
+    render();
   } else {
     console.log("Loading~");
   }
@@ -93,8 +95,8 @@ var reset = function() {
   player.x = canvas.width / 2;
   player.y = canvas.height / 2;
 
-  goblin.x = mathRandom(600);
-  goblin.y = mathRandom(600);
+  goblin.x = mathRandom(canvas.width - 50);
+  goblin.y = mathRandom(canvas.height - 50);
 }
 
 ///////////////////////////////////////////////////////////////
@@ -102,10 +104,10 @@ var reset = function() {
 // UPDATE GAME STATE //
 
 var update = function(modifier) {
-  if (38 in keysDown) player.y -= player.speed * modifier; // UP
-  if (40 in keysDown) player.y += player.speed * modifier; // DOWN
-  if (37 in keysDown) player.x -= player.speed * modifier; // LEFT
-  if (39 in keysDown) player.x += player.speed * modifier; // RIGHT
+  if (87 in keysDown) player.y -= player.speed * modifier; // UP
+  if (83 in keysDown) player.y += player.speed * modifier; // DOWN
+  if (65 in keysDown) player.x -= player.speed * modifier; // LEFT
+  if (68 in keysDown) player.x += player.speed * modifier; // RIGHT
 
   if (player.x <= (goblin.x + 32)
       && goblin.x <= (player.x + 32)
@@ -122,7 +124,46 @@ var update = function(modifier) {
 // RENDER GRAPHICS //
 
 var render = function() {
+  var scale = 2;
+  var playerWidth = playerSprite.width / 4;
+  var playerHeight = playerSprite.height / 4;
 
+  var goblinWidth = goblinSprite.width / 12;
+  var goblinHeight = goblinSprite.height / 8;
+  var goblinScale = 1.25;
+
+  // Player scale - 512x512
+  // Goblin scale - 576x384
+
+  if (loaded) {
+    ctx.drawImage(bkgd, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(playerSprite, 0, 0, playerWidth, playerHeight, player.x, player.y, playerWidth / scale, playerHeight / scale);
+    ctx.drawImage(goblinSprite, 0, 0, goblinWidth, goblinHeight, goblin.x, goblin.y, goblinWidth * goblinScale, goblinHeight * goblinScale);
+  }
+
+  ctx.fillStyle = "rgb(255,255,255)";
+  ctx.font = "bold 30px sans-serif";
+  ctx.textAlign = "left";
+  ctx.textBaseline = "top";
+  ctx.fillText(`Goblins Caught: ${caught}`, 10, 10);
+  ctx.strokeStyle = "rgb(0, 0, 0)";
+  ctx.strokeText(`Goblins Caught: ${caught}`, 10, 10);
+}
+
+///////////////////////////////////////////////////////////////
+
+// GAME LOOP
+
+var main = function() {
+  var now = Date.now();
+  var delta = now - then;
+
+  update(delta / 1000);
+  render();
+
+  then = now;
+  console.log("then:" + delta);
+  requestAnimationFrame(main);
 }
 
 ///////////////////////////////////////////////////////////////
@@ -133,3 +174,8 @@ function mathRandom(max) {
   return Math.ceil(Math.random() * max);
   console.log(Math.ceil(Math.random() * max));
 }
+
+
+var then = Date.now();
+reset();
+main();
